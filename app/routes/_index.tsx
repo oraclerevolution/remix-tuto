350044d5064fd262c24ef44905ba4edd9f9bff26
oraclerevolution <mac.n.drix@gmail.com>
2024-04-29 13:43:34 +0000

    init and test remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Index(){
+    return(
+        <p id="index-page">
+            This is a demo for remix made by <a href="https://www.linkedin.com/in/assia-jean-gontran/">Assia Jean N'Goran</a>.
+            <br />
+            Check out{" "}
+            <a href="https://github.com/oraclerevolution/remix-tuto">the code at github</a>
+        </p>
+    );
+}
